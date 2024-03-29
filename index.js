83899ba9819c4c443690bb6e0325b4bafe9bd870
paul449 <paul_bilbatua@hotmail.com>
2024-03-29 10:55:17 -0500

    index.js

---
+++ index.js
@@ -0,0 +1,50 @@
+const { name } = require('ci-info');
+const {readFile, writeFile} = require('fs/promises');
+const inquirer = require('inquirer');
+
+/*
+GIVEN a command-line application that accepts user input
+WHEN I am prompted for text
+THEN I can enter up to three characters
+WHEN I am prompted for the text color
+THEN I can enter a color keyword (OR a hexadecimal number)
+WHEN I am prompted for a shape
+THEN I am presented with a list of shapes to choose from: circle, triangle, and square
+WHEN I am prompted for the shape's color
+THEN I can enter a color keyword (OR a hexadecimal number)
+WHEN I have entered input for all the prompts
+THEN an SVG file is created named `logo.svg`
+AND the output text "Generated logo.svg" is printed in the command line
+WHEN I open the `logo.svg` file in a browser
+THEN I am shown a 300x200 pixel image that matches the criteria I entered
+*/
+const questions = [
+
+    {
+        type:'input',
+        name:'name',
+        message:'What name would you like for your logo (up to three characters):'
+    },
+    {
+        type:'input',
+        name:'text-color',
+        message: 'What color would you like for your logo name?:'
+    },
+    {
+        type:'list',
+        name:'shape',
+        choices:["circle","triangle","square"],
+        message: 'What shape would you like for your logo?(only circle, triangle, and square):'
+    },
+    {
+        type:'input',
+        name:'shape-color',
+        message: 'What color would you like for your shape background?:'
+    }
+
+]
+
+inquirer.prompt(questions)
+.then((answers)=>{
+    console.log(answers)
+})
